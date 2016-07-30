95a9011a6a46068d2d598e6eca87ab25b4f016d1
pospa <pospa@pospa.net>
2016-07-30 09:54:31 -0700

    Configuration class added

---
+++ MSBandDispatcher/Configuration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Devices.Client;
+
+namespace Pospa.NET.SmartOffice.MSBandDispatcher
+{
+    public static class Configuration
+    {
+        private const string EventHubHostname = "<Put your EventHub Hostname here>";
+        private const string DeviceId = "<Put your Device ID here>";
+        private const string DeviceToken = "<Put your SAS key here>";
+
+        public static class EventHub
+        {
+            public static string Hostname => EventHubHostname;
+            public static IAuthenticationMethod Authentication => new DeviceAuthenticationWithToken(DeviceId, DeviceToken);
+
+            public static TransportType TransportType => TransportType.Amqp;
+        }
+    }
+}

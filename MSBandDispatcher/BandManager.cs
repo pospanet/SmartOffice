404190c24badaed7407fa42eb06978f18b6ecc66
pospa <pospa@pospa.net>
2016-07-29 17:45:42 -0700

    read.me

--- MSBandDispatcher/BandManager.cs
+++ MSBandDispatcher/BandManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Data.Json;
+using Windows.Security.ExchangeActiveSyncProvisioning;
+using Microsoft.Azure.Devices.Client;
 using Microsoft.Band;
 using Microsoft.Band.Sensors;
 
@@ -16,6 +19,13 @@ namespace Pospa.NET.SmartOffice.MSBandDispatcher
         public int LastValue { get; private set; }
         public bool ReadingLocked { get; private set; }
 
+        private static readonly EasClientDeviceInformation ClientDeviceInformation;
+
+        static BandManager()
+        {
+            ClientDeviceInformation = new EasClientDeviceInformation();
+        }
+
         public BandManager(IBandInfo band, IBandClient client)
         {
             Band = band;
@@ -25,10 +35,20 @@ namespace Pospa.NET.SmartOffice.MSBandDispatcher
             Client.SensorManager.HeartRate.ReadingChanged += HeartRate_ReadingChanged;
         }
 
-        private void HeartRate_ReadingChanged(object sender, Microsoft.Band.Sensors.BandSensorReadingEventArgs<Microsoft.Band.Sensors.IBandHeartRateReading> e)
+        private async void HeartRate_ReadingChanged(object sender, Microsoft.Band.Sensors.BandSensorReadingEventArgs<Microsoft.Band.Sensors.IBandHeartRateReading> e)
         {
             LastValue = e.SensorReading.HeartRate;
             ReadingLocked = e.SensorReading.Quality == HeartRateQuality.Locked;
+            await SendDataToCloud();
+        }
+
+        private async Task SendDataToCloud()
+        {
+            MSBandData data = new MSBandData(LastValue, ReadingLocked, Band.Name, ClientDeviceInformation.FriendlyName);
+            DeviceClient client = Microsoft.Azure.Devices.Client.DeviceClient.Create();
+            Message message=new Message();
+            Json.Convert
+            await client.SendEventAsync(message);
         }
 
         public override string ToString()
---
+++ MSBandDispatcher/MSBandData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Pospa.NET.SmartOffice.MSBandDispatcher
+{
+    public class MSBandData
+    {
+        public MSBandData()
+        {
+            TimeStamp = DateTime.UtcNow;
+        }
+
+        public MSBandData(int heartRate, bool readingLocked, string bandName, string brokerName) : this()
+        {
+            HeartRate = heartRate;
+            DataLocked = readingLocked;
+            BandName = bandName;
+            BrokerName = brokerName;
+        }
+
+        public string BandName { get; set; }
+        public string BrokerName { get; set; }
+        public int HeartRate { get; set; }
+        public bool DataLocked { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
